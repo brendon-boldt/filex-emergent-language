31a41977c7aab4b381ab28c710d0a554b18c3e68
Brendon Boldt <brendon.mb@protonmail.com>
2021-07-28 20:32:24 -0400

    Remove optimal.py and iter_groups

--- nav_to_center/analyze.py
+++ nav_to_center/analyze.py
@@ -1,49 +1,13 @@
 import argparse
 from pathlib import Path
-from typing import Any, List, Optional, Tuple, Iterator, Dict, Union
-from itertools import product
-import math
+from typing import Any, List, Tuple, Dict, Union
 
 from scipy.stats import linregress  # type: ignore
 from matplotlib import pyplot as plt  # type: ignore
-import matplotlib  # type: ignore
 import numpy as np  # type: ignore
 import pandas as pd  # type: ignore
 
 from . import analysis_configs
-
-
-def iter_groups(
-    df: pd.DataFrame, groups: List[str], plot_shape: Optional[Tuple[int, int]]
-) -> Iterator[Tuple[List, pd.DataFrame, matplotlib.axes.Axes]]:
-    valss = product(*(df[groups[i]].unique() for i in range(len(groups))))
-    for vals in valss:
-        filtered = df.loc[(df[groups] == vals).all(1)]
-        if not len(filtered):
-            continue
-        if plot_shape is not None:
-            filtered = filtered[: plot_shape[0] * plot_shape[1]]
-            # random_idxs = np.random.default_rng().choice(
-            #     len(filtered),
-            #     min(len(filtered), np.prod(plot_shape)),
-            #     replace=False,
-            # )
-            # filtered = filtered.iloc[random_idxs]
-        else:
-            row_len = math.ceil(math.sqrt(len(filtered)))
-            plot_shape = row_len, row_len
-            # figsize = (8, 8)
-        figsize = 4 * plot_shape[1], 4 * plot_shape[0]
-        fig, axes = plt.subplots(*plot_shape, figsize=figsize)
-        plt.subplots_adjust(
-            left=0,
-            right=1.0,
-            top=1,
-            bottom=0,
-            wspace=0,
-            hspace=0,
-        )
-        yield vals, filtered, axes
 
 
 def analyze_correlation(df: pd.DataFrame, cfg: Dict[str, Any]) -> None:
